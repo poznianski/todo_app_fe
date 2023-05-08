fbc1e27b68863283b39289a688115a104335114e
poznianski <poznianski@gmail.com>
2023-05-08 11:53:10 +0300

    fixed layout, added server link

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,36 +1,38 @@
-import axios from 'axios'
-import { Todo } from '../types/Todo'
-import { FilterType } from '../types/FilterType'
+import axios from 'axios';
+import { Todo } from '../types/Todo';
+import { FilterType } from '../types/FilterType';
 
-axios.defaults.baseURL = 'http://localhost:5501'
+axios.defaults.baseURL = 'https://todo-app-be.onrender.com';
 
 export function getAllTodos(type?: FilterType): Promise<Todo[]> {
-  return axios.get('/todos').then((res) => res.data)
+  return axios.get('/todos').then((res) => res.data);
 }
 
 export async function updateAllTodos(todos: Todo[]): Promise<Todo[]> {
   const res = await axios.patch('/todos?action=update', {
     items: todos,
-  })
-  return res.data
+  });
+  return res.data;
 }
 
 export function deleteAllTodos(ids: number[]): Promise<Todo[]> {
-  return axios.patch('/todos?action=delete', { ids }).then((res) => res.data)
+  return axios.patch('/todos?action=delete', { ids }).then((res) => res.data);
 }
 
 export function getOneTodo(todoId: string): Promise<Todo> {
-  return axios.get(`/todos/${todoId}`).then((res) => res.data)
+  return axios.get(`/todos/${todoId}`).then((res) => res.data);
 }
 
 export function addTodo(title: string): Promise<Todo> {
-  return axios.post('/todos', { title }).then((res) => res.data)
+  return axios.post('/todos', { title }).then((res) => res.data);
 }
 
 export function removeTodo(todoId: number): Promise<string> {
-  return axios.delete(`/todos/${todoId}`).then((res) => res.data)
+  return axios.delete(`/todos/${todoId}`).then((res) => res.data);
 }
 
 export function updateTodo({ id, title, completed }: Todo): Promise<Todo> {
-  return axios.put(`/todos/${id}`, { title, completed }).then((res) => res.data)
+  return axios
+    .put(`/todos/${id}`, { title, completed })
+    .then((res) => res.data);
 }
--- src/components/Auth/AuthForm.tsx
+++ src/components/Auth/AuthForm.tsx
@@ -4,7 +4,7 @@ import { createUser, getUserByEmail } from '../../api/users';
 import { User } from '../../types/User';
 
 export type Props = {
-  onLogin: (user: User) => void,
+  onLogin: (user: User) => void;
 };
 
 export const AuthForm: React.FC<Props> = ({ onLogin }) => {
@@ -45,8 +45,7 @@ export const AuthForm: React.FC<Props> = ({ onLogin }) => {
     }
   };
 
-  const registerUser = () => createUser({ name, email })
-    .then(saveUser);
+  const registerUser = () => createUser({ name, email }).then(saveUser);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -68,48 +67,15 @@ export const AuthForm: React.FC<Props> = ({ onLogin }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="box mt-5">
-      <h1 className="title is-3">
-        {needToRegister ? 'You need to register' : 'Log in to open todos'}
-      </h1>
+    <div className="container-wrapper">
+      <form onSubmit={handleSubmit} className="box mt-5">
+        <h1 className="title is-3">
+          {needToRegister ? 'You need to register' : 'Log in to open todos'}
+        </h1>
 
-      <div className="field">
-        <label className="label" htmlFor="user-email">
-          Email
-        </label>
-
-        <div
-          className={classNames('control has-icons-left', {
-            'is-loading': loading,
-          })}
-        >
-          <input
-            type="email"
-            id="user-email"
-            className={classNames('input', {
-              'is-danger': !needToRegister && errorMessage,
-            })}
-            placeholder="Enter your email"
-            disabled={loading || needToRegister}
-            value={email}
-            required
-            onChange={(e) => setEmail(e.target.value)}
-          />
-
-          <span className="icon is-small is-left">
-            <i className="fas fa-envelope" />
-          </span>
-        </div>
-
-        {!needToRegister && errorMessage && (
-          <p className="help is-danger">{errorMessage}</p>
-        )}
-      </div>
-
-      {needToRegister && (
         <div className="field">
-          <label className="label" htmlFor="user-name">
-            Your Name
+          <label className="label" htmlFor="user-email">
+            Email
           </label>
 
           <div
@@ -118,40 +84,75 @@ export const AuthForm: React.FC<Props> = ({ onLogin }) => {
             })}
           >
             <input
-              type="text"
-              id="user-name"
+              type="email"
+              id="user-email"
               className={classNames('input', {
-                'is-danger': needToRegister && errorMessage,
+                'is-danger': !needToRegister && errorMessage,
               })}
-              placeholder="Enter your name"
+              placeholder="Enter your email"
+              disabled={loading || needToRegister}
+              value={email}
               required
-              minLength={4}
-              disabled={loading}
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <span className="icon is-small is-left">
-              <i className="fas fa-user" />
+              <i className="fas fa-envelope" />
             </span>
           </div>
 
-          {needToRegister && errorMessage && (
+          {!needToRegister && errorMessage && (
             <p className="help is-danger">{errorMessage}</p>
           )}
         </div>
-      )}
 
-      <div className="field">
-        <button
-          type="submit"
-          className={classNames('button is-primary', {
-            'is-loading': loading,
-          })}
-        >
-          {needToRegister ? 'Register' : 'Login'}
-        </button>
-      </div>
-    </form>
+        {needToRegister && (
+          <div className="field">
+            <label className="label" htmlFor="user-name">
+              Your Name
+            </label>
+
+            <div
+              className={classNames('control has-icons-left', {
+                'is-loading': loading,
+              })}
+            >
+              <input
+                type="text"
+                id="user-name"
+                className={classNames('input', {
+                  'is-danger': needToRegister && errorMessage,
+                })}
+                placeholder="Enter your name"
+                required
+                minLength={4}
+                disabled={loading}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+
+              <span className="icon is-small is-left">
+                <i className="fas fa-user" />
+              </span>
+            </div>
+
+            {needToRegister && errorMessage && (
+              <p className="help is-danger">{errorMessage}</p>
+            )}
+          </div>
+        )}
+
+        <div className="field">
+          <button
+            type="submit"
+            className={classNames('button is-primary', {
+              'is-loading': loading,
+            })}
+          >
+            {needToRegister ? 'Register' : 'Login'}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
